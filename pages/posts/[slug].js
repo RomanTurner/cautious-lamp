fb3cb099410e0780da4d5991634247accf8cad24
Roman Turner <rn.turner@live.com>
2021-06-14 23:08:01 -0700

    router practice

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { useRouter } from 'next/router';
 
 function PostShow() {
+    const router = useRouter();
+    const { slug } = router.query
+
     return (
-        <div>
-            
-        </div>
+     <p>Post: {slug}</p>
     )
 }
 
