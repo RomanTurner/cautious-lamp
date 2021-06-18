2f18338cddca620a72eb5d481329ef44e2f425ae
Roman Turner <rn.turner@live.com>
2021-06-17 23:08:06 -0700

    drawer work

--- components/Drawer.js
+++ components/Drawer.js
@@ -16,6 +16,9 @@ export default Drawer =() => {
           <li>list item </li>
           <li>list item </li>
           <li>list item </li>
+          <li>list item </li>
+          <li>list item </li>
+          <li>list item </li>
         </ul>
       </div>
     );
